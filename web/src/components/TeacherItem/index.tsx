9fe9c2640ec57a4db485091dcc09531fbcd1eb6a
MatheusHNSantos <matheushnsantos.97@gmail.com>
2020-08-07 02:05:33 -0300

    Day #03

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,35 +1,57 @@
 import React from 'react';
+import api from '../../services/api';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-function TeacherItem() {
+
+export interface Teacher {
+  id: number;
+  subject: string;
+  cost: number;
+  name: string;
+  avatar: string;
+  whatsapp: string;
+  bio: string;
+}
+
+ interface TeacherItemProps {
+  teacher: Teacher; 
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    });
+  }
+  
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://media-exp1.licdn.com/dms/image/C4D03AQHoiRfm04dbeg/profile-displayphoto-shrink_200_200/0?e=1602115200&v=beta&t=1lLQ_wWyUse2eqQg4hRjiDZxgkKxsmgDLEe_xbWSEvg" alt="Matheus Henrique"/>
+        <img src={teacher.avatar} alt={teacher.name}/>
         <div>
-          <strong>Matheus Henrique</strong>
-          <span>História</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Teste Teste Teste TesteTeste TesteTeste TesteTeste.
-        <br/> <br/>
-        Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a 
+          target="_blank"
+          onClick={createNewConnection} 
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="Whatsapp"/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
 
     </article>
